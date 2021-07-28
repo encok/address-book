a799f888fdac48fc8b8b4ff10ef8662aaa7ed4fe
enock <enckkipronoh@gmail.com>
2021-07-28 15:38:32 +0300

    adding the address

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,36 @@
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
+    this.addresses = [];
+  }
+  function Addresess(street, city, county){
+    this.street = street;
+    this.city = city;
+    this.county = county;
+  }
+  Contact.prototype.fullName = function(){
+    return this.firstName + "   " + this.lastName;
   }
 // user interface logic
 $(document).ready(function(){
   $("form#new-contact").submit(function(event){
     event.preventDefault();
 
-    var inputtedFirstName = $("#input#new-first-name").val();
+    var inputtedFirstName = $("input#new-first-name").val();
 
     var inputtedLastName = $("input#new-last-name").val ();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
    
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName()+ "</span></li>");
+    
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      // $("#show-contact h2").text(newContact.lastName);
+      $(".first-name"). text(newContact.firstName);
+      $(".last-name").text (newContact.lastName);
+    });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
